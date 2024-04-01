22176850a30127b8afd71c8fe55c852d4420272e
Jakaria Hossain <mdjak8980@gmail.com>
2024-04-01 12:58:28 +0600

    bitmask dp problem added

--- DP/Bitmask DP ( CF 1950G ).cpp
+++ DP/Bitmask DP ( CF 1950G ).cpp
@@ -1,3 +1,4 @@
+///https://codeforces.com/contest/1950/problem/G
 #include<bits/stdc++.h>
 using namespace std;
 #define FIO            ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
